36c76d5a45f73cd68cee13c217832dbdceb2a784
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-02 12:38:17 +0300

    commit tree e06e6dfb9a11da28013c8121b63949803024fe04

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function add(a,b){
+    return a + b;
+}
+console.log(add(623562,7634));
+
+
+function subtract(a,b){
+    return a -b;
+}
+console.log(subtract(623562,7634));
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(623562,7634));
+
+function divide(a,b){
+    return a/b;
+}
+console.log(divide(623562,7634));
+
+function increment(n){
+    return n +=1;
+}
+console.log(increment(10));
+
+function decrement(n){
+    return n -=1;
+}
+console.log(decrement(20));
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+console.log(makeInt("874934.8494803"));
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+console.log(preserveDecimal("985754.08448"))
